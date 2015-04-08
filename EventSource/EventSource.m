f6ac2418976fc874084630c9a5ebe2b707ae75fa
Oleg Komaristov <oleg.komaristov@gmail.com>
2015-04-08 17:05:45 +0300

    Multiply events in one response support.

--- EventSource/EventSource.m
+++ EventSource/EventSource.m
@@ -33,6 +33,7 @@ static NSString *const ESEventRetryKey = @"retry";
 @property (nonatomic, assign) NSTimeInterval timeoutInterval;
 @property (nonatomic, assign) NSTimeInterval retryInterval;
 @property (nonatomic, strong) id lastEventID;
+@property (nonatomic, strong) NSMutableData *dataBuffer;
 
 - (void)open;
 
@@ -63,7 +64,8 @@ static NSString *const ESEventRetryKey = @"retry";
         _eventURL = URL;
         _timeoutInterval = timeoutInterval;
         _retryInterval = ES_RETRY_INTERVAL;
-        
+        _dataBuffer = [NSMutableData new];
+      
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(_retryInterval * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
             [self open];
@@ -112,6 +114,91 @@ static NSString *const ESEventRetryKey = @"retry";
     [self.eventSource cancel];
 }
 
+- (BOOL)isString:(NSString *)string containSuffix:(NSString *)suffix location:(NSInteger *)suffixLocation {
+  NSInteger suffixIdx = [string rangeOfString:suffix options:NSBackwardsSearch].location;
+  if (suffixIdx != NSNotFound && suffixLocation) {
+      *suffixLocation = suffixIdx;
+  }
+  return (suffixIdx != NSNotFound);
+}
+
+- (void)processCompleteEvents
+{
+    NSString *eventString = [[NSString alloc] initWithData:self.dataBuffer encoding:NSUTF8StringEncoding];
+    
+    NSArray *eventSeparators = @[ESEventSeparatorLFLF, ESEventSeparatorCRCR, ESEventSeparatorCRLFCRLF];
+    __block BOOL isContainEvents = NO;
+    __block NSInteger lastEventSuffixIdx = NSNotFound;
+    __block NSString *eventSeparator = nil;
+    [eventSeparators enumerateObjectsUsingBlock:^(NSString *suffix, NSUInteger idx, BOOL *stop) {
+        if ([self isString:eventString containSuffix:suffix location:&lastEventSuffixIdx]) {
+            eventSeparator = suffix;
+            *stop = isContainEvents = YES;
+        }
+    }];
+    
+    if (isContainEvents) {
+        if (lastEventSuffixIdx < eventString.length - eventSeparator.length - 1) { // contain incomplete event
+            NSString *incompleteEvent = [eventString substringFromIndex:lastEventSuffixIdx + eventSeparator.length];
+            [self.dataBuffer setData:[incompleteEvent dataUsingEncoding:NSUTF8StringEncoding]];
+        }
+        else {
+            [self.dataBuffer setData:[NSData new]];
+        }
+        eventString = [eventString substringToIndex:lastEventSuffixIdx];
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            NSArray *eventsList = [eventString componentsSeparatedByString:eventSeparator];
+            [eventsList enumerateObjectsUsingBlock:^(NSString *eventString, NSUInteger idx, BOOL *stop) {
+                NSMutableArray *components = [[eventString componentsSeparatedByString:ESEventKeyValuePairSeparator] mutableCopy];
+                
+                Event *e = [Event new];
+                e.readyState = kEventStateOpen;
+                
+                for (NSString *component in components) {
+                    if (component.length == 0) {
+                        continue;
+                    }
+                    
+                    NSInteger index = [component rangeOfString:ESKeyValueDelimiter].location;
+                    if (index == NSNotFound || index == (component.length - 2)) {
+                        continue;
+                    }
+                    
+                    NSString *key = [component substringToIndex:index];
+                    NSString *value = [component substringFromIndex:index + ESKeyValueDelimiter.length];
+                    
+                    if ([key isEqualToString:ESEventIDKey]) {
+                        e.id = value;
+                        self.lastEventID = e.id;
+                    } else if ([key isEqualToString:ESEventEventKey]) {
+                        e.event = value;
+                    } else if ([key isEqualToString:ESEventDataKey]) {
+                        e.data = value;
+                    } else if ([key isEqualToString:ESEventRetryKey]) {
+                        self.retryInterval = [value doubleValue];
+                    }
+                }
+                
+                NSArray *messageHandlers = self.listeners[MessageEvent];
+                for (EventSourceEventHandler handler in messageHandlers) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        handler(e);
+                    });
+                }
+                
+                if (e.event != nil) {
+                    NSArray *namedEventhandlers = self.listeners[e.event];
+                    for (EventSourceEventHandler handler in namedEventhandlers) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            handler(e);
+                        });
+                    }
+                }
+            }];
+        });
+    }
+}
+
 // ---------------------------------------------------------------------------------------------------------------------
 
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
@@ -152,64 +239,15 @@ static NSString *const ESEventRetryKey = @"retry";
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
-    __block NSString *eventString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    
-    if ([eventString hasSuffix:ESEventSeparatorLFLF] ||
-        [eventString hasSuffix:ESEventSeparatorCRCR] ||
-        [eventString hasSuffix:ESEventSeparatorCRLFCRLF]) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-            eventString = [eventString stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
-            NSMutableArray *components = [[eventString componentsSeparatedByString:ESEventKeyValuePairSeparator] mutableCopy];
-            
-            Event *e = [Event new];
-            e.readyState = kEventStateOpen;
-            
-            for (NSString *component in components) {
-                if (component.length == 0) {
-                    continue;
-                }
-                
-                NSInteger index = [component rangeOfString:ESKeyValueDelimiter].location;
-                if (index == NSNotFound || index == (component.length - 2)) {
-                    continue;
-                }
-                
-                NSString *key = [component substringToIndex:index];
-                NSString *value = [component substringFromIndex:index + ESKeyValueDelimiter.length];
-                
-                if ([key isEqualToString:ESEventIDKey]) {
-                    e.id = value;
-                    self.lastEventID = e.id;
-                } else if ([key isEqualToString:ESEventEventKey]) {
-                    e.event = value;
-                } else if ([key isEqualToString:ESEventDataKey]) {
-                    e.data = value;
-                } else if ([key isEqualToString:ESEventRetryKey]) {
-                    self.retryInterval = [value doubleValue];
-                }
-            }
-            
-            NSArray *messageHandlers = self.listeners[MessageEvent];
-            for (EventSourceEventHandler handler in messageHandlers) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    handler(e);
-                });
-            }
-            
-            if (e.event != nil) {
-                NSArray *namedEventhandlers = self.listeners[e.event];
-                for (EventSourceEventHandler handler in namedEventhandlers) {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        handler(e);
-                    });
-                }
-            }
-        });
-    }
+    [self.dataBuffer appendData:data];
+    [self processCompleteEvents];
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
+    [self processCompleteEvents];
+    [self.dataBuffer setData:[NSData new]];
+  
     if (wasClosed) {
         return;
     }
